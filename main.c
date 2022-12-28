5718a4b44220a267458035b24b2a627c3f553ceb
Leandro Rener <121558742+Leandrolrb@users.noreply.github.com>
2022-12-28 11:38:01 -0400

    Using the concept of type creation in C (typedef)

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
@@ -24,5 +24,3 @@ int main()
 
     return 0;
 }
-
-
